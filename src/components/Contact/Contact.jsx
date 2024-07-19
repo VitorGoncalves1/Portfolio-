ec7e556c0ed36447e846414c63a95bb585d554b7
Vitor Gonçalves <vagcavaco@sga.pucminas.br>
2024-07-19 16:52:36 -0300

    up

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,24 +7,24 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contato</h2>
+        <p>Sinta-se à vontade para entrar em contato!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:vitor.agc@outlook.com">vitor.agc@outlook.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/vitor-gon%C3%A7alves-/">Vitor Gonçalves</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/VitorGoncalves1">Vitor Gonçalves</a>
         </li>
       </ul>
     </footer>
